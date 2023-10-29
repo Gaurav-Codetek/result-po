6f51d5230ed2d80610a9df0dea88a081a8580795
Gaurav Patel <gp43883@gmail.com>
2023-10-29 22:24:54 +0530

    Input type for password Changed

--- src/Components/FormSection.js
+++ src/Components/FormSection.js
@@ -48,7 +48,7 @@ function FormSection() {
                     <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-12 lg:pl-14 w-4">
                         <span className="text-gray-500 sm:text-sm"><RiLockPasswordFill size="20" color='#0a7fd5' /></span>
                     </div>
-                    <input type="text" name="username" id="username" className="block w-76 rounded-3xl border-0 py-2 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-inset focus:ring-sky-900 sm:text-sm sm:leading-6" placeholder="Password" />
+                    <input type="password" name="username" id="username" className="block w-76 rounded-3xl border-0 py-2 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-inset focus:ring-sky-900 sm:text-sm sm:leading-6" placeholder="Password" />
                 </div>
             </div>
 
