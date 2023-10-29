63716538472cb724c1a922f3bd62a9c335d079c1
Gaurav Patel <gp43883@gmail.com>
2023-10-29 19:48:31 +0530

    Result Portal with Tailwind, Responsiveness, Cross Browser Suppot

---
+++ src/Components/Bg.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import logo from './Images/UIET_logo.png'
+import FormSection from './FormSection';
+import {BsArrowRight} from 'react-icons/bs'
+
+function Bg() {
+    return (
+        <>
+            <div className=" bg-img bg-cov h-screen blur-sm relative"></div>
+            <div className="h-fit absolute border top-1/2 left-2/4 trans-50-revert flex w-9/12 md:w-11/12 lg:w-9/12 rounded-r-md rounded-l-md shadow-2xl" >
+                <div className="bg-white w-full md:w-2/5 flex-col items-center justify-center rounded-l-md rounded-r-md md:rounded-r-none shadow-2xl ">
+                    <div className="flex items-center justify-center">
+                        <img className="h-32 w-32 md:h-36 md:w-36  mt-2 md:mt-4" src={logo} alt="UIET-logo" />
+                    </div>
+                    <div className="font-bold text-xl leading-10">UIET Panjab University</div>
+                    <div className="font-extrabold text-lg lg:mb-5 lg:text-xl leading-10 text-blue-950">RESULT PORTAL</div>
+                    <FormSection />
+                </div>
+                <div className="w-3/5 hidden md:block bg-slate-50/40 rounded-r-md flex-col items-center justify-center">
+                    <div className="">
+                        <p className="text-3xl mt-6"> Notices & Updates</p>
+                    </div>
+                    <div className="border-md border border-slate-950 mx-32 lg:mx-52 my-4"></div>
+                    <br />
+                    <div className="flex justify-center items-center mx-2 my-4">
+                        <div className="w-fit bg-sky-600/70 py-2 px-3 rounded-md text-white mx-10">
+                            <p className="txt-xl">23</p>
+                            <p className="txt-sm">aug</p>
+                        </div>
+                        <div className="w-4/5 text-left">
+                            <p className="font-semibold text-xl">Result update!</p>
+                            <p className="font-normal">2nd year results uploaded.</p>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="flex justify-center items-center mx-2 my-4">
+                        <div className="w-fit bg-sky-600/70 py-2 px-3 rounded-md text-white mx-10">
+                            <p className="txt-xl">23</p>
+                            <p className="txt-sm">aug</p>
+                        </div>
+                        <div className="w-4/5 text-left">
+                            <p className="font-semibold text-xl">Result update!</p>
+                            <p className="font-normal">2nd year results uploaded.</p>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="flex justify-center items-center mx-2 my-4">
+                        <div className="w-fit bg-sky-600/70 py-2 px-3 rounded-md text-white mx-10">
+                            <p className="txt-xl">23</p>
+                            <p className="txt-sm">aug</p>
+                        </div>
+                        <div className="w-4/5 text-left">
+                            <p className="font-semibold text-xl">Result update!</p>
+                            <p className="font-normal">2nd year results uploaded.</p>
+                        </div>
+                    </div>
+
+                    <div className="w-11/12 flex items-center justify-end">
+                        <p className="text-right cursor-pointer hover:underline mx-1">Previous</p>
+                        <BsArrowRight className="mt-1"/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Bg
---
+++ src/Components/FormSection.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { FaUser } from 'react-icons/fa'
+import { MdContacts } from 'react-icons/md'
+import { RiLockPasswordFill } from 'react-icons/ri'
+
+
+function FormSection() {
+    const [roles, setRoles] = useState('Select Roles')
+
+    const onSelect = () => {
+        let a = document.querySelector('#roles');
+        var rolesValue = a.value;
+
+        if (rolesValue === "Roles") {
+            setRoles('Select Roles');
+        }
+        else if (rolesValue === "Student") {
+            setRoles('Student');
+        }
+        else if (rolesValue === "Faculty") {
+            setRoles('Faculty');
+        }
+        else if (rolesValue === "Admin") {
+            setRoles('Admin')
+        }
+        // else{
+        //     setRoles('Select Roles')
+        // }
+    }
+    return (
+        <>
+            {/* -------------UserName--------------- */}
+            <div>
+                <div className="relative mt-2 flex justify-center rounded-md  mb-5">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-12 lg:pl-14 w-4">
+                        <span className="text-gray-500 sm:text-sm"><FaUser size="20" color='#0a7fd5' /></span>
+                    </div>
+                    <input type="text" name="username" id="username" className="block w-76 rounded-3xl border-0 py-2 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-inset focus:ring-sky-900 sm:text-sm sm:leading-6" placeholder="User ID" />
+                </div>
+            </div>
+
+            {/* ------------Password---------------- */}
+            <div>
+                <div className="relative mt-2 flex justify-center rounded-md mb-5">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-12 lg:pl-14 w-4">
+                        <span className="text-gray-500 sm:text-sm"><RiLockPasswordFill size="20" color='#0a7fd5' /></span>
+                    </div>
+                    <input type="text" name="username" id="username" className="block w-76 rounded-3xl border-0 py-2 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-inset focus:ring-sky-900 sm:text-sm sm:leading-6" placeholder="Password" />
+                </div>
+            </div>
+
+            {/* ------------Select Roles---------------- */}
+            <div>
+                <div className="relative mt-2 flex justify-center rounded-md mb-5">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-12 lg:pl-14 w-4">
+                        <span className="text-gray-500 sm:text-sm"><MdContacts size="20" color='#0a7fd5' /></span>
+                    </div>
+                    <input type="text" name="price" id="price" disabled className="block w-76 rounded-3xl border-0 py-2 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-inset focus:ring-sky-900 sm:text-sm sm:leading-6" placeholder={roles} />
+                    <div className="absolute inset-y-0 right-0 flex mr-10 border-l  items-center ">
+                        <select id="roles" onClick={onSelect} name="roles" defaultValue="Select Roles" className="h-full rounded-md border-0 cursor-pointer bg-transparent py-0 lg:mr-2 pl pr text-gray-500 focus:outline-none sm:text-sm">
+                            <option value="Roles">Roles</option>
+                            <option value="Student">Student</option>
+                            <option value="Faculty">Faculty</option>
+                            <option value="Admin">Admin</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            {/* --------------Submit Button---------------- */}
+            <button className="rounded-full bg-sky-600 py-1 px-4 text-slate-50 w-2/5 hover:bg-sky-700 mb-3">Log In</button>
+            <h3 className="text-sm mb-4 hover:text-sky-700 cursor-pointer hover:underline" >Forgot Password ?</h3>
+        </>
+    );
+}
+
+export default FormSection
